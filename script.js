5affd3253c31457f508e89fa695e3f725a4e82c8
Bvanoo <bvanoostveldt@gmail.com>
2026-06-19 16:05:12 +0200

    Refactor HTML forms and implement shopping cart functionality in JavaScript

--- script.js
+++ script.js
@@ -47,27 +47,42 @@
 
 //Calculer un devis et une tva 
 
-//Simuler un panier d'achat avec des articles et leurs prix, puis calculer le total et la TVA applicable.
 
 
 //calculer la moyenne des notes .
 
-const inp = document.querySelector("input");
+// const inp = document.querySelector("input");
 
 
-inp.addEventListener("change",(e) => {
-    if(e.target.value === "vert"){
-        inp.classList.remove(...inp.classList);
-        inp.classList.add("green")
-    }else if(e.target.value ==="rouge"){
-        inp.classList.remove(...inp.classList);
-        inp.classList.add('red');
-    }else if(e.target.value === "bleu"){
-        inp.classList.remove(...inp.classList);
-        inp.classList.add('blue')
-    }else{
-        inp.classList.remove(...inp.classList);
-    }
+// inp.addEventListener("change",(e) => {
+//     if(e.target.value === "vert"){
+//         inp.classList.remove(...inp.classList);
+//         inp.classList.add("green")
+//     }else if(e.target.value ==="rouge"){
+//         inp.classList.remove(...inp.classList);
+//         inp.classList.add('red');
+//     }else if(e.target.value === "bleu"){
+//         inp.classList.remove(...inp.classList);
+//         inp.classList.add('blue')
+//     }else{
+//         inp.classList.remove(...inp.classList);
+//     }
+// })
+
+
+//Simuler un panier d'achat avec des articles et leurs prix, puis calculer le total et la TVA applicable.
+
+const bask = document.querySelectorAll(".panier");
+const btn = document.querySelector("button");
+const p = document.querySelector("p");
+const total = document.getElementById("result");
+let result = "Vous avez choisis : <br>";
+let tot= 0;
+
+btn.addEventListener("click",()=>
+{
+    tot += bask[1].value*bask[2].value*1.21;
+    result +=`${bask[0].value} au prix de ${bask[1].value} htva x ${bask[2].value} pour un total de ${bask[1].value*bask[2].value*1.21} TVAC <br>`
+    p.innerHTML= result
+    total.innerHTML = tot;
 })
-
-
